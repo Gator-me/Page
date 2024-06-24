d204aa4060dc1e0e07635a99f5898b3dfc11b86f
Gerald Rush <gtrush4@gmail.com>
2024-06-23 18:50:30 -0700

    Adjusting placement and adding tab icon

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function handleClick() {
     console.info("click");
-    var elem = document.getElementById("buttondiv");
+    var elem = document.getElementById("maindiv");
     var bg = window.getComputedStyle(elem, null).getPropertyValue("background-color");
     if (bg == "rgb(0, 0, 0)") {
         console.info("bg");
