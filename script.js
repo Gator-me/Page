47f632b1222269a78b6c299e5ae9df47acd8b1c4
Gerald Rush <gtrush4@gmail.com>
2024-06-23 17:49:45 -0700

    Toggle colors

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function handleClick() {
-    alert("This is a message");
+    console.info("click");
+    var elem = document.getElementById("buttondiv");
+    var bg = window.getComputedStyle(elem, null).getPropertyValue("background-color");
+    if (bg == "rgb(0, 0, 0)") {
+        console.info("bg");
+        elem.style.backgroundColor = "aquamarine";
+    } else {
+        console.info(bg);
+        elem.style.backgroundColor = "black";
+    }
 }
